34ca415ef14dbe19207c9b6470c9ac7c9e5b6044
Simon Kämpe <simon@linetic.se>
2022-07-14 22:24:32 +0200

    Add full name

--- users/simon.nix
+++ users/simon.nix
@@ -3,6 +3,7 @@
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.simon = {
+    description = "Simon Kämpe";
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
     password = "changethis";
