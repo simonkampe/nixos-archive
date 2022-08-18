22a1e982037d5c5a5ad2157adea71a8fbdf217d4
Simon Kämpe <simon@linetic.se>
2022-08-18 22:01:35 +0200

    Add VPN

--- suites/linetic.nix
+++ suites/linetic.nix
@@ -3,6 +3,13 @@
   environment.systemPackages = with pkgs; [
     kmail
     teams
+    globalprotect-openconnect
   ];
+
+  services.globalprotect = {
+    enable = true;
+    # if you need a Host Integrity Protection report
+    csdWrapper = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
+  };
 }
 
