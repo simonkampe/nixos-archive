c361887ea0260475d38c5c1fcd4a12f47674a00a
Simon Kämpe <simon.kampe@gmail.com>
2022-11-12 18:20:24 +0100

    Add Veloren launcher

--- hosts/mendelevium.nix
+++ hosts/mendelevium.nix
@@ -32,6 +32,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    airshipper
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
