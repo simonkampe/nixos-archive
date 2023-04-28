f68878e15aaf20f8a1105d8eb196f8db49af7235
Simon Kämpe <simon@linetic.se>
2023-04-28 07:46:48 +0200

    Add some laser stuff

--- suites/making.nix
+++ suites/making.nix
@@ -1,10 +1,17 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    snapmaker-luban
+    master.snapmaker-luban
     blender
     freecad
     gimp
-    inkscape
+    master.inkscape
+    master.lightburn
+    master.k40-whisperer
   ];
+
+  # Udev for laser cutter
+  services.udev.extraRules = ''
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="5512", ENV{DEVTYPE}="usb_device", GROUP="users", MODE="0777"
+  '';
 }
