c2f12d1fbb540c2baea174cc1fd2f1ee7e704d8f
Simon Kämpe <simon.kampe@gmail.com>
2023-05-26 16:45:08 +0200

    Add protonup

--- suites/gaming.nix
+++ suites/gaming.nix
@@ -5,5 +5,9 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+
+  environment.systemPackages = with pkgs; [
+    protonup-ng
+  ];
 }
 
