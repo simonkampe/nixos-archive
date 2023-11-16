c2359f6b6cb39707cedf7fe272783d765292c160
Simon Kämpe <simon@linetic.se>
2023-11-16 08:53:29 +0100

    Firejail to support multiple outlooks/teams

--- suites/office.nix
+++ suites/office.nix
@@ -7,7 +7,25 @@
     samba
     remmina # RDP client
     teamviewer
+    chromium
   ];
+
+  programs.firejail = {
+    enable = true;
+
+    wrappedBinaries = {
+      kauto = {
+        executable = "${pkgs.chromium}/bin/chromium https://outlook.office.com https://teams.microsoft.com";
+        profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
+        extraArgs = [ "--private=~/.local/share/firejail/chromium-kauto" ];
+      };
+      esab = {
+        executable = "${pkgs.chromium}/bin/chromium https://outlook.office.com https://teams.microsoft.com";
+        profile = "${pkgs.firejail}/etc/firejail/chromium.profile";
+        extraArgs = [ "--private=~/.local/share/firejail/chromium-esab" ];
+      };
+    };
+  };
 
   services = {
     onedrive.enable = true;
