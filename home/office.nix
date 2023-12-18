76f725beacb66e196fcc30a49a2eb62823038c25
Simon Kämpe <simon@linetic.se>
2023-12-18 07:11:11 +0100

    Fix firejailed stuff

--- home/office.nix
+++ home/office.nix
@@ -5,7 +5,7 @@
     ka-office = {
       name = "KA Office";
       genericName = "Web Browser";
-      exec = ''firejail --private="~/.local/share/firejail/chromium-kauto" ${pkgs.chromium}/bin/chromium https://outlook.office.com https://teams.microsoft.com'';
+      exec = ''kauto-office'';
       terminal = false;
       icon = "chromium";
       categories = [ "Application" "Office" ];
@@ -14,7 +14,7 @@
     esab-office = {
       name = "ESAB Office";
       genericName = "Web Browser";
-      exec = ''firejail --private="~/.local/share/firejail/chromium-esab" ${pkgs.chromium}/bin/chromium https://outlook.office.com https://teams.microsoft.com'';
+      exec = ''esab-office'';
       terminal = false;
       icon = "chromium";
       categories = [ "Application" "Office" ];
